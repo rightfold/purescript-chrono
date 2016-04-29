278bb2214d456a36b0befededff1b854d15fd7d8
rightfold <rightfold@gmail.com>
2016-04-29 20:27:19 +0200

    Add LocalDate smart constructor

--- src/Data/Chrono/LocalDate.purs
+++ src/Data/Chrono/LocalDate.purs
@@ -1,11 +1,32 @@
 module Data.Chrono.LocalDate
-( LocalDate(LocalDate)
+( LocalDate
+, localDate
+, localDateEra
+, localDateYear
+, localDateMonth
+, localDateDay
 ) where
 
 import Data.Chrono.Gregorian (Era, Month, Year)
+import Data.Maybe (Maybe(Just, Nothing))
 
 data LocalDate = LocalDate { era :: Era
                            , year :: Year
                            , month :: Month
                            , day :: Int
                            }
+
+localDate :: Era -> Year -> Month -> Int -> Maybe LocalDate
+localDate era year month day = Just (LocalDate {era, year, month, day})
+
+localDateEra :: LocalDate -> Era
+localDateEra (LocalDate {era}) = era
+
+localDateYear :: LocalDate -> Year
+localDateYear (LocalDate {year}) = year
+
+localDateMonth :: LocalDate -> Month
+localDateMonth (LocalDate {month}) = month
+
+localDateDay :: LocalDate -> Int
+localDateDay (LocalDate {day}) = day
