07c8b6726f8b5ae7ba00a664ad62c77216c21c7f
rightfold <rightfold@gmail.com>
2016-04-29 18:52:46 +0200

    Add umbrella module

---
+++ src/Data/Chrono.purs
@@ -0,0 +1,13 @@
+module Data.Chrono
+( module Data.Chrono.Arithmetic
+, module Data.Chrono.Clock
+, module Data.Chrono.Duration
+, module Data.Chrono.Instant
+, module Data.Chrono.Period
+) where
+
+import Data.Chrono.Arithmetic
+import Data.Chrono.Clock
+import Data.Chrono.Duration
+import Data.Chrono.Instant
+import Data.Chrono.Period
